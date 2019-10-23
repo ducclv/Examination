ec65f12ecf5a3072f8ba3ab1ff935d22673afa6a
leducuet <leducuet@gmail.com>
2019-10-23 17:06:23 +0700

    status bar

--- App/Components/SignUp.js
+++ App/Components/SignUp.js
@@ -7,6 +7,7 @@ import {
   ScrollView,
   SafeAreaView,
   Navigation,
+  StatusBar,
 } from 'react-native';
 import RadioForm, {
   RadioButton,
@@ -21,10 +22,16 @@ var radio_props = [
   { label: "Nữ", value: "nu" },
 ];
 
-
 export default class SignUp extends Component {
   static navigationOptions = {
-    title: 'Đăng ký tài khoản'
+    title: 'Đăng ký tài khoản',
+    headerStyle: {
+      height: 70,
+    },
+    headerTitleStyle: {
+      fontWeight: 'bold',
+      marginTop: 10,
+    },
   };
   constructor(props) {
     super(props);
@@ -43,101 +50,104 @@ export default class SignUp extends Component {
 
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <ScrollView style={styles.scrollView}>
-          <View style={styles.container}>
-            <View style={styles.title}>
-              <Text style={styles.title}>Thông tin tài khoản</Text>
-            </View>
+      <>
+        <SafeAreaView style={styles.container}>
+          <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent={true} />
+          <ScrollView style={styles.scrollView}>
+            <View style={styles.container}>
+              <View style={styles.title}>
+                <Text style={styles.title}>Thông tin tài khoản</Text>
+              </View>
 
-            <TextInput
-              style={styles.input}
-              underlineColorAndroid="transparent"
-              placeholder="ID đăng nhập"
-              placeholderTextColor="#808080"
-              autoCapitalize="none"
-              keyboardType="email-address"
-              onChangeText={username => this.setState({ username })}
-              value={this.state.username}
-            />
-
-            <View style={styles.passwordViewContainer}>
               <TextInput
                 style={styles.input}
                 underlineColorAndroid="transparent"
-                placeholder="Mật khẩu"
+                placeholder="ID đăng nhập"
                 placeholderTextColor="#808080"
                 autoCapitalize="none"
-                onChangeText={this.handlePassword}
-                secureTextEntry={this.state.showPassword}
-                value={this.state.password}
+                keyboardType="email-address"
+                onChangeText={username => this.setState({ username })}
+                value={this.state.username}
               />
-              <Icon style={styles.icon}
-                name={this.state.icEye}
-                size={23}
-                onPress={this.changePwdType}
+
+              <View style={styles.passwordViewContainer}>
+                <TextInput
+                  style={styles.input}
+                  underlineColorAndroid="transparent"
+                  placeholder="Mật khẩu"
+                  placeholderTextColor="#808080"
+                  autoCapitalize="none"
+                  onChangeText={this.handlePassword}
+                  secureTextEntry={this.state.showPassword}
+                  value={this.state.password}
+                />
+                <Icon style={styles.icon}
+                  name={this.state.icEye}
+                  size={23}
+                  onPress={this.changePwdType}
+                />
+              </View>
+              <View style={styles.title}>
+                <Text style={styles.title}>Thông tin cá nhân</Text>
+              </View>
+              <View style={styles.gender}>
+                <Text>Giới tính</Text>
+                <RadioForm
+                  style={styles.btnGender}
+                  radio_props={radio_props}
+                  initial={-1}
+                  onPress={(value) => { this.state.gender }}
+                  buttonSize={10}
+                  labelStyle={{ marginRight: 30 }}
+                />
+              </View>
+              <TextInput
+                style={styles.input}
+                underlineColorAndroid="transparent"
+                placeholder="Họ và tên"
+                placeholderTextColor="#808080"
+                autoCapitalize="none"
+                onChangeText={fullname => this.setState({ fullname })}
+                value={this.state.fullname}
               />
-            </View>
-            <View style={styles.title}>
-              <Text style={styles.title}>Thông tin cá nhân</Text>
-            </View>
-            <View style={styles.gender}>
-              <Text>Giới tính</Text>
-              <RadioForm
-                style={styles.btnGender}
-                radio_props={radio_props}
-                initial={-1}
-                onPress={(value) => { this.state.gender }}
-                buttonSize={10}
-                labelStyle={{ marginRight: 30 }}
+              <TextInput
+                style={styles.input}
+                underlineColorAndroid="transparent"
+                placeholder="Số di động"
+                placeholderTextColor="#808080"
+                keyboardType="number-pad"
+                autoCapitalize="none"
+                onChangeText={phonenumber => this.setState({ phonenumber })}
+                value={this.state.phonenumber}
               />
+              <TextInput
+                style={styles.input}
+                underlineColorAndroid="transparent"
+                placeholder="Ngày sinh"
+                placeholderTextColor="#808080"
+                autoCapitalize="none"
+                onChangeText={birthday => this.setState({ birthday })}
+                value={this.state.birthday}
+              />
+              <TextInput
+                style={styles.input}
+                underlineColorAndroid="transparent"
+                placeholder="Email"
+                placeholderTextColor="#808080"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={email => this.setState({ email })}
+                value={this.state.email}
+              />
+              <TouchableOpacity
+                style={styles.submitButton}
+                onPress={this.gotoSignIn}>
+                <Text style={styles.submitButtonText}> Đăng ký </Text>
+              </TouchableOpacity>
             </View>
-            <TextInput
-              style={styles.input}
-              underlineColorAndroid="transparent"
-              placeholder="Họ và tên"
-              placeholderTextColor="#808080"
-              autoCapitalize="none"
-              onChangeText={fullname => this.setState({ fullname })}
-              value={this.state.fullname}
-            />
-            <TextInput
-              style={styles.input}
-              underlineColorAndroid="transparent"
-              placeholder="Số di động"
-              placeholderTextColor="#808080"
-              keyboardType="number-pad"
-              autoCapitalize="none"
-              onChangeText={phonenumber => this.setState({ phonenumber })}
-              value={this.state.phonenumber}
-            />
-            <TextInput
-              style={styles.input}
-              underlineColorAndroid="transparent"
-              placeholder="Ngày sinh"
-              placeholderTextColor="#808080"
-              autoCapitalize="none"
-              onChangeText={birthday => this.setState({ birthday })}
-              value={this.state.birthday}
-            />
-            <TextInput
-              style={styles.input}
-              underlineColorAndroid="transparent"
-              placeholder="Email"
-              placeholderTextColor="#808080"
-              keyboardType="email-address"
-              autoCapitalize="none"
-              onChangeText={email => this.setState({ email })}
-              value={this.state.email}
-            />
-            <TouchableOpacity 
-              style={styles.submitButton} 
-              onPress={this.gotoSignIn}>
-              <Text style={styles.submitButtonText}> Đăng ký </Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+          </ScrollView>
+        </SafeAreaView>
+      </>
     );
   }
   changePwdType = () => {
@@ -166,7 +176,7 @@ export default class SignUp extends Component {
     };
     this.setState(newState);
   };
-  gotoSignIn=()=>{
+  gotoSignIn = () => {
     this.props.navigation.navigate('SignIn');
   };
 }
